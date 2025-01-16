782e6c4aacd2da54273f94e6fb372ac98f381942
alja667 <aprogozhina@gmail.com>
2025-01-17 00:05:26 +0300

    Update module_12_3.py

--- module_12_3.py
+++ module_12_3.py
@@ -24,18 +24,40 @@ class TournamentTest(unittest.TestCase):
         name_1 = runnner_2.Tournament(90, self.obj1, self.obj3)
         all_results = name_1.start()
         self.assertTrue(all_results[2], self.obj3)
+        if all_results[2] != "Ник":
+            print(f'Ошибка! Последним должен быть Ник')
+        else:
+            print(f"Ошибки нет! Последний Ник")
 
 
     def test_2(self):
         name_2 = runnner_2.Tournament(90, self.obj2, self.obj3)
         all_results = name_2.start()
         self.assertTrue(all_results[2], self.obj3)
+        if all_results[2] != "Ник":
+            print(f'Ошибка! Последним должен быть Ник')
+        else:
+            print(f"Ошибки нет! Последний Ник")
+
 
     def test_3(self):
         name_3 = runnner_2.Tournament(90, self.obj1, self.obj2, self.obj3)
         all_results = name_3.start()
         self.assertTrue(all_results[3], self.obj3)
+        if all_results[3] != "Ник":
+            print(f'Ошибка! Последним должен быть Ник')
+        else:
+            print(f"Ошибки нет! Последний Ник")
 
+    def test_4 (self):
+        name_3 = runnner_2.Tournament(6, self.obj1, self.obj2, self.obj3)
+        all_results = name_3.start()
+        self.assertTrue(all_results[3], self.obj3)
+        if all_results[3] != "Ник":
+            print(f'Ошибка! Последним должен быть Ник')
+        else:
+            print(f"Ошибки нет! Последний Ник")
+            
 if __name__ == '__main__':
     unittest.main()
 
